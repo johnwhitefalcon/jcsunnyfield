e0cde81ba2646179839e7d204fe975c6a8276d61
John Carlisle <johncarlislework@gmail.com>
2023-08-06 19:11:16 +1000

    fix margins

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,13 @@ Disability Sector Workforce Analysis
 
 </div>
 
-<div className="text-3xl fixed z-10 ml-[-75rem] mt-[-30rem] w-[20rem]">
+<div className="text-3xl fixed z-10 ml-[-65rem] mt-[-30rem] w-[20rem]">
     <img src="https://b2504970.smushcdn.com/2504970/wp-content/uploads/sunnyfield-disability-services-logo.png?lossy=0&strip=1&webp=1"/>
 
 </div>
 
 
-<div className="text-3xl fixed z-10 ml-[76rem] mt-[-30rem] w-[20rem]" >
+<div className="text-3xl fixed z-10 ml-[66rem] mt-[-30rem] w-[20rem]" >
     <img src="https://alliance20.org.au/wp-content/uploads/2018/06/alliance_20_logo.png"/>
 
 </div>
